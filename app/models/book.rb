8c9c21db80c0a60eb004640726b4927a4c796ade
Gitanjali Mule <gitanjalidmule@gmail.com>
2019-07-15 12:27:01 -0500

    established relations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,4 @@
 class Book < ApplicationRecord
+	has_many :notes
 	validates :book, presence: true
 end
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,4 +1,7 @@
 class Note < ApplicationRecord
+	belongs_to :user
+	belongs_to :book
+
 	validates :content, presence: true , length: {in: 1..200}
 	validates :public, presence: true
 end
