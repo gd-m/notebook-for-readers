bcdc5ea3dda143bff6aacbf9cc0fd2ca261a7522
Gitanjali Mule <gitanjalidmule@gmail.com>
2019-07-15 10:39:55 -0500

    minnor changes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "sessions#welcome"
   get '/home', to: "sessions#home" , as: 'home'
-  devise_for :users, :controllers => {registrations: ‘registrations’ }
+  devise_for :users, :controllers => {registrations: 'registrations' }
 
 end
