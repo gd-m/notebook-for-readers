8b5cdba744fe6088c9088213d719584f82dec0da
Gitanjali Mule <gitanjalidmule@gmail.com>
2019-07-15 10:52:42 -0500

    registration controller is not working so had to remove it from routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "sessions#welcome"
   get '/home', to: "sessions#home" , as: 'home'
-  devise_for :users, :controllers => {registrations: 'registrations' }
-
+  devise_for :users
 end
