d1a892062823699af601725e3d720dd4bc580176
Gitanjali Mule <gitanjalidmule@gmail.com>
2019-07-15 13:12:11 -0500

    added new, show and create methods

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,37 @@
 class BooksController < ApplicationController
+	def new
+		@book = Book.new	
+	end
+
+	def show
+		@book = Book.find(params[:id])
+	end
+
+	def create
+		@book = Book.create(book_params)
+
+		redirect_to book_path(@book)
+
+	end
+
+	def index
+		
+	end
+	
+	def edit
+		
+	end
+
+	def update
+		
+	end
+
+	def destroy
+	end
+
+	private
+
+	def book_params
+		params.require(:book).permit(:name)
+	end
 end
