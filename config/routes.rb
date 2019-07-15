6a0570ea5096513dfd5c6d9db22f868c84af68ee
Gitanjali Mule <gitanjalidmule@gmail.com>
2019-07-15 10:38:17 -0500

    added root path and setup user registration contoller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "sessions#welcome"
+  get '/home', to: "sessions#home" , as: 'home'
+  devise_for :users, :controllers => {registrations: ‘registrations’ }
+
 end
