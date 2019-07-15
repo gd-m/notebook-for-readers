8c412a411765638b0d7731795d2fc3c7ea032c32
Gitanjali Mule <gitanjalidmule@gmail.com>
2019-07-15 10:41:28 -0500

    added home and welcome methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,18 @@
 class SessionsController < ApplicationController
+	def home
+		if user_signed_in?
+			@user = current_user
+			render :home
+		else
+			redirect_to welcome_path
+		end
+	end
+
+	def welcome
+		if user_signed_in?
+			redirect_to home_path
+		else
+			render :welcome
+		end
+	end
 end
