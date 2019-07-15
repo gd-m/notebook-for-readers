64cfc6c59153ad561766419ef72f12ed0dac7da2
Gitanjali Mule <gitanjalidmule@gmail.com>
2019-07-15 13:19:52 -0500

    added notes_params private method

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,35 @@
 class NotesController < ApplicationController
+	def index
+		
+	end
+
+	def new
+		
+	end
+
+	def create
+		
+	end
+
+	def show
+		
+	end
+
+	def edit
+		
+	end
+
+	def update
+		
+	end
+
+	def destroy
+		
+	end
+
+	private
+
+	def notes_params
+		params.require(:note).permit(:content, :public)
+	end
 end
